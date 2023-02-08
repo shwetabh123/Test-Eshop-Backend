0bcb064daab44424ea6b04be402bd14fcb554fa4
shwetabh123 <31085825+shwetabh123@users.noreply.github.com>
2023-02-08 07:11:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ const path = require("path");
 // }
 
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(_dirname,"./frontrnd/build")));
+ if (process.env.NODE_ENV === "production") {
+   app.use(express.static(path.join(_dirname,"./frontend/build")));
   
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-//   });
-// }
+   app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+   });
+ }
 
 
 // app.get("/", (req, res) => {
